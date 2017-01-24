31a5e7fd21cfa0ced41fc3c0c7df55a669744e5c
Ben Toews <mastahyeti@gmail.com>
2017-01-24 12:55:52 -0700

    Initial Commit

---
+++ SoftU2FTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SoftU2FTool
+//
+//  Created by Benjamin P Toews on 1/24/17.
+//  Copyright © 2017 GitHub. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ SoftU2FTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SoftU2FTool
+//
+//  Created by Benjamin P Toews on 1/24/17.
+//  Copyright © 2017 GitHub. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
